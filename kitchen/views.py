8cca19bbbc893931ea91a14d05eac98f9c341215
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-07-21 15:51:13 +0300

    Add home page view

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Cook, Dish, DishType
+
+
+@login_required
+def index(request):
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
