189f8da9f2813b946fb88809f463a31a272d320d
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-07-21 16:47:06 +0300

    Add auth user model and login redirect url to the settings

--- restaurant_kitchen_django/settings.py
+++ restaurant_kitchen_django/settings.py
@@ -101,6 +101,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "kitchen.Cook"
+
+LOGIN_REDIRECT_URL = "/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
