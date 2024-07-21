21666136e30343d3e197bde9bd1114433f0496be
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-07-21 15:30:58 +0300

    Add the Cook model

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class DishType(models.Model):
@@ -9,3 +11,17 @@ class DishType(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(unique=True)
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("kitchen:cook-detail", kwargs={"pk": self.pk})
