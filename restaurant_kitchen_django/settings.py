5d78b80f78fdd8bbe8c29ee2b23d1585868a4360
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-07-27 21:53:32 +0300

    Add info to README.md file

--- restaurant_kitchen_django/settings.py
+++ restaurant_kitchen_django/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9+g11=%t^mwqfp9)vdq=_8+!(xo^a)kv7*_q-v^6-!y3j(ps$r"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
